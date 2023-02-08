aeb1262eb7ccaa3d5e06127e0f6b0bb780a63294
Jenish Potsangbam <jenish.potsangbam@hcl.com>
2023-02-08 23:07:52 +0530

    I commit

---
+++ text_generator/text-front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainComponent from './component/main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
